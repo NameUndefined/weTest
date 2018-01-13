8bc5ba51eff9e3f09746134e998199c9231a0cb6
wangyuan <a@a.com>
2018-01-13 05:42:22 +0000

    s

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -6,7 +6,7 @@ score = 0;
 trans = {0:'a',1:'b',2:'c',3:'d',4:'e',5:'f',6:'g',7:'h',8:'i',
             'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8};
 SERVER_IP = 'http://township.ink:8001';
-SERVER_IP = 'http://localhost:5000';
+//SERVER_IP = 'http://localhost:5000';
 
 function debuger(x) {
     if (debug)
